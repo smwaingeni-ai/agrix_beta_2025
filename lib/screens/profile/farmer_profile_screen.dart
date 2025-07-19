f9ddebd411c2837e14b6127ce439af1f08ac9bfb
smwaingeni-ai <smwaingeni.ai@gmail.com>
2025-07-19 22:35:01 +0300

    Update farmer_profile_screen.dart

--- lib/screens/profile/farmer_profile_screen.dart
+++ lib/screens/profile/farmer_profile_screen.dart
@@ -1,12 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:agrix_beta_2025/models/farmer_profile.dart';
+import '../models/farmer_profile.dart';
 
 class FarmerProfileService {
   static const String _fileName = 'farmer_profiles.json';
@@ -39,7 +36,6 @@ class FarmerProfileService {
     final json = jsonEncode(profiles.map((p) => p.toJson()).toList());
     await file.writeAsString(json);
 
-    // Set as active
     await setActiveProfile(profile);
   }
 
