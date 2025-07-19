64b3f852cb2279e31ef9edfd53b110ff7d8976e0
smwaingeni-ai <smwaingeni.ai@gmail.com>
2025-07-19 22:54:26 +0300

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/profile/farmer_profile_screen.dart';
-import 'screens/profile/edit_farmer_profile_screen.dart';
+import 'package:agrix_beta_2025/screens/profile/farmer_profile_screen.dart';
+import 'package:agrix_beta_2025/screens/profile/edit_farmer_profile_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   '/profile': (context) => const FarmerProfileScreen(),
