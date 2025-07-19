34ea2dea5a87bcdfea62eccf7ac29ccf74560be9
smwaingeni-ai <smwaingeni.ai@gmail.com>
2025-07-20 00:10:02 +0300

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:agrix_beta_2025/screens/profile/farmer_profile_screen.dart';
 import 'package:agrix_beta_2025/screens/profile/edit_farmer_profile_screen.dart';
 // Add others as implemented
-import 'package:agrix_beta_2025/screens/market/market_screen.dart';
-import 'package:agrix_beta_2025/screens/loans/loan_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   '/profile': (context) => const FarmerProfileScreen(),
   '/profile/edit': (context) => const EditFarmerProfileScreen(),
-  '/market': (context) => const MarketScreen(),
-  '/loan': (context) => const LoanScreen(),
-  // Add others once available
+  // '/market': (context) => const MarketScreen(), // To be added later
+  // '/loan': (context) => const LoanScreen(),     // To be added later
 };
