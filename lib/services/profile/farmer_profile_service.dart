11444e23ecabe7b8b8225d65924a52792f7a80fb
smwaingeni-ai <smwaingeni.ai@gmail.com>
2025-07-19 23:42:06 +0300

    Update farmer_profile_service.dart

--- lib/services/profile/farmer_profile_service.dart
+++ lib/services/profile/farmer_profile_service.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 
-import '../../models/farmer_profile.dart';
+import 'package:agrix_beta_2025/models/farmer_profile.dart';
 
 class FarmerProfileService {
   static const String _fileName = 'farmer_profiles.json';
