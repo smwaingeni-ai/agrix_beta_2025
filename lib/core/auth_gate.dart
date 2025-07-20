e89bed264d8c08a727d24864d3597b06d7b87f5f
smwaingeni-ai <smwaingeni.ai@gmail.com>
2025-07-20 11:49:38 +0300

    Create auth_gate.dart

---
+++ lib/core/auth_gate.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:agrix_beta_2025/screens/auth/login_screen.dart';
+import 'package:agrix_beta_2025/screens/core/landing_page.dart';
+import 'package:agrix_beta_2025/services/session_service.dart';
+
+class AuthGate extends StatefulWidget {
+  const AuthGate({super.key});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+  bool _isLoading = true;
+  bool _isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkSession();
+  }
+
+  Future<void> _checkSession() async {
+    final sessionExists = await SessionService.checkSession();
+    setState(() {
+      _isLoggedIn = sessionExists;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoading) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    return _isLoggedIn ? const LandingPage() : const LoginScreen();
+  }
+}
