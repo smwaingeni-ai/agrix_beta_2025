1f4d1627521cd20391b8daace3505c264838bfbc
smwaingeni-ai <smwaingeni.ai@gmail.com>
2025-07-21 21:14:23 +0300

    Create sustainability_log_service.dart

---
+++ lib/services/sustainability/sustainability_log_service.dart
@@ -0,0 +1,36 @@
+// lib/services/sustainability/sustainability_log_service.dart
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:agrix/models/sustainability/sustainability_log.dart';
+
+class SustainabilityLogService {
+  static const String _key = 'sustainability_logs';
+
+  Future<List<SustainabilityLog>> loadLogs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonString = prefs.getString(_key);
+    if (jsonString != null) {
+      final List<dynamic> jsonList = json.decode(jsonString);
+      return jsonList
+          .map((jsonItem) => SustainabilityLog.fromJson(jsonItem))
+          .toList();
+    }
+    return [];
+  }
+
+  Future<void> saveLog(SustainabilityLog log) async {
+    final prefs = await SharedPreferences.getInstance();
+    final logs = await loadLogs();
+    logs.add(log);
+    final jsonString = json.encode(logs.map((l) => l.toJson()).toList());
+    await prefs.setString(_key, jsonString);
+  }
+
+  Future<void> deleteLog(int index) async {
+    final prefs = await SharedPreferences.getInstance();
+    final logs = await loadLogs();
+    logs.removeAt(index);
+    final jsonString = json.encode(logs.map((l) => l.toJson()).toList());
+    await prefs.setString(_key, jsonString);
+  }
+}
