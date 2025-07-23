8a0052b7ec92ce027f1767b0962fa00a3fc75508
smwaingeni-ai <smwaingeni.ai@gmail.com>
2025-07-23 21:18:16 +0300

    Create trader_service.dart

---
+++ lib/services/trader/trader_service.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import '../../models/trader/trader_profile.dart';
+
+class TraderService {
+  static const String _dataPath = 'assets/data/traders.json';
+
+  Future<List<TraderProfile>> loadTraderProfiles() async {
+    final jsonString = await rootBundle.loadString(_dataPath);
+    final List<dynamic> jsonData = json.decode(jsonString);
+    return jsonData.map((json) => TraderProfile.fromJson(json)).toList();
+  }
+
+  Future<TraderProfile?> getTraderById(String id) async {
+    final profiles = await loadTraderProfiles();
+    return profiles.firstWhere((trader) => trader.traderId == id, orElse: () => null);
+  }
+}
